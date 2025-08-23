1dd35f4d6d020845f6ca5c25fb56d49ac95177bf
Victor Biga <victor.biga@gmail.com>
2025-08-23 11:43:05 +0100

    Update k3s-config.nix

--- shared/k3s-config.nix
+++ shared/k3s-config.nix
@@ -10,9 +10,11 @@ in
 
 {
   services.k3s = {
+    enable = true;
     role = role;
 
     serverAddr = if role == "agent" then serverAddr else "";
+    clusterInit = if role == "server" then clusterInit else false;
 
     extraFlags = (if role == "server" then [ "--disable=traefik" ] else []) ++ labelFlags;
   };
